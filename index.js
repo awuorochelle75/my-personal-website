35b7aa601774a5ffc7622a62169d3c65e6b63aa5
Rochelle Awuor <awuorochelle75@gmail.com>
2025-03-02 18:55:51 +0300

    updates index.html,index.js,css/

---
+++ index.js
@@ -0,0 +1,11 @@
+ let navPoint = document.querySelectorAll("nav ul li" );
+
+    navPoint.forEach(function(point){
+        point.addEventListener("mouseover", function(){
+        point.style.backgroundColor ="#ffd70";
+        });
+
+        point.addEventListener("mouseout",function(){
+            point.style.backgroundColor ="#ffd70";
+        });
+    });
